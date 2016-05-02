cd3fc9a0df97a1360186a56746bc18e15bedb6a1
Leilenah Mamea <leilenahmamea@yahoo.com>
2016-05-01 18:21:20 -0700

    Added query of full db that limits 11 and offsets 14

--- skills.sql
+++ skills.sql
@@ -24,13 +24,27 @@ WHERE m.name = 'Mustang';
 
 -- 5. Select all rows for the three oldest brands
 --    from the Brands table (Hint: you can use LIMIT and ORDER BY).
-
+SELECT * from brands
+ORDER BY founded
+LIMIT 3;
 
 -- 6. Count the Ford models in the database (output should be a number).
+SELECT COUNT(brand_name)
+FROM models
+WHERE brand_name = 'Ford';
 
 -- 7. Select the name of any and all car brands that are not discontinued.
+SELECT name FROM brands
+WHERE discontinued IS NULL;
 
 -- 8. Select rows 15-25 of the DB in alphabetical order by model name.
+SELECT *
+FROM models AS m
+JOIN brands AS b
+ON m.brand_name = b.name
+ORDER BY m.name
+LIMIT 11
+OFFSET 14;
 
 -- 9. Select the brand, name, and year the model's brand was
 --    founded for all of the models from 1960. Include row(s)
